fbbe219efaef6e416840e075911b9e59b3dde5c9
Eduard Benedic <benediceduard@outlook.com>
2021-11-25 18:19:14 +0000

    .

--- src/index.js
+++ src/index.js
@@ -3,14 +3,38 @@ const { ApolloServer } = require('apollo-server')
 const typeDefs = `
     type Query {
         info: String!
+        feed: [Link!]!
+    }
+    type Link {
+        id: ID!
+        description: String!
+        url: String!
     }
 `
 
+let links = [
+    {
+        id: 'link-0',
+        description: 'Hello',
+        url: 'https://google.com'
+    }
+]
+
 const resolvers = {
     Query: {
-        info: () => `My first resolver GraphQL implementation`
+        info: () => `My first resolver GraphQL implementation`,
+        feed: () => links
+    },
+    Link: {
+        id: (parent) => {
+            console.log(parent)
+            return parent.id
+        },
+        description: (parent) => parent.description,
+        url: (parent) => parent.url
     }
 }
+
 
 const server = new ApolloServer({
     typeDefs,
